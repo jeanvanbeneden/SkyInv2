351e196293e656db17258ef3495e32f1a6be1890
edouard-dsmdt <edouard.desmedt@icloud.com>
2024-05-04 23:54:04 +0200

    amélioration menu

--- app/src/main/java/com/example/skyinv2/MainActivity.kt
+++ app/src/main/java/com/example/skyinv2/MainActivity.kt
@@ -7,15 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.TextView
 
 
 class  MainActivity : AppCompatActivity() {
     private lateinit var play : Button
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
         window.setFlags(
